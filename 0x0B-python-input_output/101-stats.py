5296dd96d322961ea26add878fd7aa3a09a4912e
theclubfoot <papakwesiadams@icloud.com>
2022-07-05 09:53:21 -0700

    l

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python3
-ads from standard input and computes metrics.
+#!/usr/bin/python3ads from standard input and computes metrics.
 
 """
 After every ten lines or the input of a keyboard interruption (CTRL + C),
@@ -11,6 +10,7 @@ prints the following statistics:
 
 def print_stats(size, status_codes):
     """Print accumulated metrics.
+
     Args:
         size (int): The accumulated read file size.
         status_codes (dict): The accumulated count of status codes.
