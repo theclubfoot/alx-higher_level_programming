7f7f98e1fdf481069ae9005550f36a5c214f339d
theclubfoot <papakwesiadams@icloud.com>
2022-10-19 12:07:12 -0700

    Fixing bugs

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
-
+// get starwars characters from a film
 const request = require('request');
-const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
-
-request(starWarsUri, function (_err, _res, body) {
-  const characters = JSON.parse(body).characters;
-
-  for (let i = 0; i < characters.length; ++i) {
-    request(characters[i], function (_cErr, _cRes, cBody) {
-	    console.log(JSON.parse(cBody).name);
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (error) throw error;
+  for (const character of JSON.parse(body).characters) {
+    request(character, function (error, response, body) {
+      if (error) throw error;
+      console.log(JSON.parse(body).name);
     });
   }
 });
--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,46 +1,13 @@
 #!/usr/bin/node
-
+// get starwars characters from a film
 const request = require('request');
-
-function getDataFrom (url) {
-  return new Promise(function (resolve, reject) {
-    request(url, function (err, _res, body) {
-	    if (err) {
-        reject(err);
-	    } else {
-        resolve(body);
-	    }
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (error) throw error;
+  for (const character of JSON.parse(body).characters) {
+    request(character, function (error, response, body) {
+      if (error) throw error;
+      console.log(JSON.parse(body).name);
     });
-  });
-}
-
-function errHandler (err) {
-  console.log(err);
-}
-
-function printMovieCharacters (movieId) {
-  const movieUri = `https://swapi-api.hbtn.io/api/films/${movieId}`;
-
-  getDataFrom(movieUri)
-    .then(JSON.parse, errHandler)
-    .then(function (res) {
-	    const characters = res.characters;
-	    const promises = [];
-
-	    for (let i = 0; i < characters.length; ++i) {
-        promises.push(getDataFrom(characters[i]));
-	    }
-
-	    Promise.all(promises)
-        .then((results) => {
-		    for (let i = 0; i < results.length; ++i) {
-            console.log(JSON.parse(results[i]).name);
-		    }
-        })
-        .catch((err) => {
-		    console.log(err);
-        });
-    });
-}
-
-printMovieCharacters(process.argv[2]);
+  }
+});
