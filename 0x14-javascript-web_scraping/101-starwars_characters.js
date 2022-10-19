c600fa3d473dd5100f70ad52c70c48cf0f06a5dd
theclubfoot <papakwesiadams@icloud.com>
2022-10-19 12:11:45 -0700

    Fixing bugs

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,13 +1,30 @@
 #!/usr/bin/node
-// get starwars characters from a film
+/* prints all characters of a Star Wars movie.
+    - the first argument is the Movie ID.
+    - displays one character name by line.
+*/
+
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (error) throw error;
-  for (const character of JSON.parse(body).characters) {
-    request(character, function (error, response, body) {
-      if (error) throw error;
-      console.log(JSON.parse(body).name);
-    });
-  }
-});
+const BASE_URL = 'https://swapi-api.hbtn.io/api';
+
+if (process.argv.length > 2) {
+  request(`${BASE_URL}/films/${process.argv[2]}/`, (err, res, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (err, res, body) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(JSON.parse(body).name);
+        });
+      }));
+
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(err => console.log(err));
+  });
+}
